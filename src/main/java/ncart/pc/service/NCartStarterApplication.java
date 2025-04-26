e837079bd51ce48f11b41f179f1b5dae7a633333
Rama N <nmaheshtekh@gmail.com>
2025-04-26 07:07:17 +0530

    initial setup

---
+++ src/main/java/ncart/pc/service/NCartStarterApplication.java
@@ -0,0 +1,12 @@
+package ncart.pc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NCartStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NCartStarterApplication.class, args);
+    }
+}
---
+++ src/main/java/ncart/pc/service/models/PhoneType.java
@@ -0,0 +1,6 @@
+package ncart.pc.service.models;
+
+import java.io.Serializable;
+
+public class PhoneType implements Serializable {
+}
