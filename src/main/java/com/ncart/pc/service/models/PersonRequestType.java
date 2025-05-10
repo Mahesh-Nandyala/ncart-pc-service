0846f2dfbbe6974783ac07edc16796362e000d0b
Rama N <nmaheshtekh@gmail.com>
2025-05-10 07:02:04 +0530

    remove all setters and getters

--- src/main/java/com/ncart/pc/service/models/PersonRequestType.java
+++ src/main/java/com/ncart/pc/service/models/PersonRequestType.java
@@ -7,24 +7,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
 
 @Data
-@Document(collection = "persons")
 public class PersonRequestType implements Serializable {
 
     @JsonProperty("payload")
     private PersonPayloadType payload;
-
-    public PersonRequestType(PersonPayloadType payload) {
-        this.payload = payload;
-    }
-
-    public PersonPayloadType getPayload() {
-        return payload;
-    }
-
-    public void setPayload(PersonPayloadType payload) {
-        this.payload = payload;
-    }
-
-    public PersonRequestType() {
-    }
 }
